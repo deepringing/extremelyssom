82e3548cdf3437c8fb1c28f47e4b9836b124be61
gimhanul <gimhanuly@gmail.com>
2023-04-29 23:17:43 +0900

    ADD :: login page (#1)

---
+++ pages/login/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '@/styles/pages/login.module.scss'
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>로그인</title>
+      </Head>
+      <div className={styles.wrap}>
+        <aside className={styles.sidebar}>
+          <img src="/images/logo.svg" alt="logo" />
+          <p>다울림</p>
+        </aside>
+        <main className={styles.main}>
+          <h1>로그인</h1>
+          <button>
+            <img src="/images/google.png" alt="google" />
+            <span>구글 계정으로 로그인</span>
+          </button>
+        </main>
+      </div>
+    </>
+  )
+}
